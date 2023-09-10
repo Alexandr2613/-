988140e033d34222231120e5777d5abedcc7d723
Alexandr2613 <128998898+Alexandr2613@users.noreply.github.com>
2023-09-11 00:43:40 +0700

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-TOKEN = '6345399063:AAFr8fNQOrpoKc8hDz8Gc0SiMI8xy1yXm2o'
-
-API_KEY = 'S1W7qFsxKt4NBqpqF8JYKiMmSq7LOHkx'
-
-keys = {
-    "рубль":"RUB",
-    "доллар":"USD",
-    "евро":"EUR"
-    }
